4b554c7ef73fc6e291ee6f0feb05b22e3250c36f
ManoloEsS <manolo.estrada.s@gmail.com>
2026-05-28 17:57:21 -0600

    feat: add salt and master key columns to migration 00001

--- nacl_backend/sql/migrations/00001_users.sql
+++ nacl_backend/sql/migrations/00001_users.sql
@@ -3,6 +3,8 @@ CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
     username TEXT NOT NULL UNIQUE,
     password_hash VARCHAR(512) NOT NULL,
+    master_key_salt VARCHAR(255),
+    encrypted_master_key TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
