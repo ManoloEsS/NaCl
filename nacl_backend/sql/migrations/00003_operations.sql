ca1ec5ad4c6d8457c215c0719e9167f7d66968a9
ManoloEsS <manolo.estrada.s@gmail.com>
2026-05-28 19:50:19 -0600

    feat: add operations migration 00003

--- nacl_backend/sql/migrations/00003_operations.sql
+++ nacl_backend/sql/migrations/00003_operations.sql
@@ -1,12 +1,15 @@
 -- +goose Up
 CREATE TABLE operations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+    user_id UUID NOT NULL,
     type TEXT NOT NULL,
     service_id UUID NOT NULL,
     description TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT fk_service FOREIGN KEY (service_id)
-    REFERENCES services(id)
+    REFERENCES services(id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose Down
