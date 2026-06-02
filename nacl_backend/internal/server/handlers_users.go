3f6fea393532bcc48c1a12757d76e6db8a52ce99
ManoloEsS <manolo.estrada.s@gmail.com>
2026-06-01 21:05:40 -0600

    feat: add generation of master key and salt to user registration process
    -Generates master key and salt
    -Salt and password are used to generate derived key to encrypt master
    key

--- nacl_backend/internal/server/handlers_users.go
+++ nacl_backend/internal/server/handlers_users.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"encoding/base64"
 	"net/http"
 
 	"github.com/ManoloEsS/NaCl/nacl_backend/internal/db"
+	"github.com/ManoloEsS/NaCl/nacl_backend/internal/encryption"
 	pkgerr "github.com/pkg/errors"
 )
 
@@ -12,6 +14,21 @@ func (s *Server) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		s.RespondWithError(w, http.StatusBadRequest, "'username' and 'password' required", err)
 		return
+	}
+
+	salt := encryption.GenerateRandomBytes(32)
+	key := encryption.GenerateRandomBytes(32)
+
+	derivedKey, err := encryption.DeriveKey(userData.Password, salt)
+	if err != nil {
+		s.RespondWithError(w, 500, "couldn't derive key", err)
+		s.Logger.Error("could not derive key", "error", err)
+	}
+
+	encryptedMasterKey, err := encryption.Encrypt(key, derivedKey)
+	if err != nil {
+		s.RespondWithError(w, 500, "could not encrypt master key", err)
+		s.Logger.Error("could not encrypt master key", "error", err)
 	}
 
 	hashedPassword, err := s.hashPassword(userData.Password)
@@ -25,12 +42,14 @@ func (s *Server) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	queries := s.Db.Queries()
-	created, err := queries.CreateUser(
-		r.Context(), db.CreateUserParams{
-			Username:     userData.Username,
-			PasswordHash: hashedPassword,
-		})
+	query := s.Db.Queries()
+	created, err := query.CreateUser(r.Context(), db.CreateUserParams{
+		Username:           userData.Username,
+		PasswordHash:       hashedPassword,
+		MasterKeySalt:      base64.StdEncoding.EncodeToString(salt),
+		EncryptedMasterKey: base64.StdEncoding.EncodeToString(encryptedMasterKey),
+	})
+
 	if err != nil {
 		s.RespondWithError(w, 500, "could not create user", err)
 		return
