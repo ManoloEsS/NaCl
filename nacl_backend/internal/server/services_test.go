0f509a3bd5d355815b42551c53017f36916d1eea
ManoloEsS <manolo.estrada.s@gmail.com>
2026-06-07 21:54:53 -0600

    test: add tests for get all services for user endpoint

--- nacl_backend/internal/server/services_test.go
+++ nacl_backend/internal/server/services_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -14,6 +15,7 @@ import (
 func TestHandlerCreateService(t *testing.T) {
 	testDB := newTestDB(t)
 	defer testDB.Close()
+	cleanupTestDB(t, testDB, "users")
 
 	server := newTestServer(t, testDB)
 
@@ -136,5 +138,191 @@ func TestHandlerCreateService(t *testing.T) {
 		})
 	}
 
-	cleanupTestDB(t, testDB, "users")
+}
+
+func TestHandlerGetAllServicesForUser(t *testing.T) {
+	testDB := newTestDB(t)
+	defer testDB.Close()
+	cleanupTestDB(t, testDB, "users", "services")
+
+	server := newTestServer(t, testDB)
+
+	testUser := "test_services_user"
+	testPass := "test_services_pass"
+
+	// create user
+	body := fmt.Sprintf(`{"username": "%s", "password": "%s"}`, testUser, testPass)
+	req := httptest.NewRequest(http.MethodPost, "/api/users", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+
+	server.HTTPServer.Handler.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusCreated, rr.Code, "user creation failed")
+
+	// login as user
+	body = fmt.Sprintf(`{"username": "%s", "password": "%s"}`, testUser, testPass)
+	req = httptest.NewRequest(http.MethodPost, "/api/login", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	rr = httptest.NewRecorder()
+
+	server.HTTPServer.Handler.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code, "login failed")
+
+	var loginResp LoginResponse
+	err := json.NewDecoder(rr.Body).Decode(&loginResp)
+	assert.NoError(t, err, "could not decode login response")
+	token := loginResp.Token
+
+	services := []struct {
+		service      string
+		username     string
+		description  string
+		password     string
+		algo         string
+		userPassword string
+	}{
+
+		{
+			"test_service_1",
+			"test_user",
+			"description",
+			"service_pass_1",
+			"aes-gcm",
+			testPass,
+		},
+		{
+			"test_service_3",
+			"test_user",
+			"description",
+			"service_pass_3",
+			"aes-gcm",
+			testPass,
+		},
+		{
+			"test_service_4",
+			"test_user",
+			"",
+			"service_pass_4",
+			"aes-gcm",
+			testPass,
+		},
+		{
+			"test_service_5",
+			"test_user",
+			"description",
+			"service_pass_5",
+			"aes-gcm",
+			testPass,
+		},
+	}
+
+	for _, service := range services {
+		serviceRequest := ServiceRequest{
+			Service:             service.service,
+			Username:            service.username,
+			Description:         service.description,
+			Password:            service.password,
+			EncryptionAlgorithm: service.algo,
+			UserPassword:        service.userPassword,
+		}
+
+		requestJSON, _ := json.Marshal(serviceRequest)
+		req := httptest.NewRequest(http.MethodPost, "/api/services", strings.NewReader(string(requestJSON)))
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+		rr := httptest.NewRecorder()
+
+		server.HTTPServer.Handler.ServeHTTP(rr, req)
+		assert.Equal(t, http.StatusCreated, rr.Code, "error crating service")
+	}
+
+	tests := []struct {
+		name      string
+		token     string
+		wantCode  int
+		wantCount int
+		setupFunc func(string) string
+	}{
+		{
+			name:      "successfully gets all services for user",
+			token:     token,
+			wantCode:  http.StatusOK,
+			wantCount: len(services),
+			setupFunc: returnSameToken,
+		},
+		{
+			name:      "unauthorized - no token",
+			token:     "",
+			wantCode:  http.StatusUnauthorized,
+			wantCount: 0,
+			setupFunc: returnSameToken,
+		},
+		{
+			name:      "unauthorized - invalid token",
+			token:     "invalid-token",
+			wantCode:  http.StatusUnauthorized,
+			wantCount: 0,
+			setupFunc: returnSameToken,
+		},
+		{
+			name:      "returns empty array when user has no services",
+			token:     token,
+			wantCode:  http.StatusOK,
+			wantCount: 0,
+			setupFunc: func(token string) string {
+				// create user
+				body := fmt.Sprintf(`{"username": "%s", "password": "%s"}`, "test_2", "test_pass2")
+				req := httptest.NewRequest(http.MethodPost, "/api/users", strings.NewReader(body))
+				req.Header.Set("Content-Type", "application/json")
+				rr := httptest.NewRecorder()
+
+				server.HTTPServer.Handler.ServeHTTP(rr, req)
+				assert.Equal(t, http.StatusCreated, rr.Code, "user creation failed")
+
+				// login as user
+				body = fmt.Sprintf(`{"username": "%s", "password": "%s"}`, "test_2", "test_pass2")
+				req = httptest.NewRequest(http.MethodPost, "/api/login", strings.NewReader(body))
+				req.Header.Set("Content-Type", "application/json")
+				rr = httptest.NewRecorder()
+
+				server.HTTPServer.Handler.ServeHTTP(rr, req)
+				assert.Equal(t, http.StatusOK, rr.Code, "login failed")
+
+				var loginResp LoginResponse
+				err := json.NewDecoder(rr.Body).Decode(&loginResp)
+				assert.NoError(t, err, "could not decode login response")
+
+				return loginResp.Token
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			test_token := tt.setupFunc(tt.token)
+			req := httptest.NewRequest(http.MethodGet, "/api/services", nil)
+			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", test_token))
+			rr := httptest.NewRecorder()
+
+			server.HTTPServer.Handler.ServeHTTP(rr, req)
+
+			bodyJSON, err := io.ReadAll(rr.Body)
+			assert.NoError(t, err, "error reading recorder body")
+			var servicesResponse []ServiceMetadataResponse
+			json.Unmarshal(bodyJSON, &servicesResponse)
+
+			assert.Equal(t, tt.wantCode, rr.Code, "unexpected status code")
+			assert.Equal(t, tt.wantCount, len(servicesResponse))
+			if tt.wantCount > 0 {
+				assert.Equal(t, services[0].service, servicesResponse[0].Service)
+				assert.Equal(t, services[0].description, servicesResponse[0].Description)
+				assert.Equal(t, services[0].algo, servicesResponse[0].EncryptionAlgorithm)
+			}
+		})
+	}
+
+}
+
+func returnSameToken(token string) string {
+	return token
 }
