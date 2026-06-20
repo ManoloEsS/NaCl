1514c38ab67dad329460a3e33caaf58b163bc435
ManoloEsS <manolo.estrada.s@gmail.com>
2026-06-19 19:43:28 -0600

    feat: add dashboard placeholder info

--- nacl_frontend/src/pages/Dashboard.tsx
+++ nacl_frontend/src/pages/Dashboard.tsx
@@ -5,6 +5,10 @@ export const Dashboard = () => {
     <Layout>
       <h2>Dashboard</h2>
       <div>Hello from dash</div>
+      <div>
+        here will go the information about the website, why encrypt passwords
+        yourself and the way it works
+      </div>
     </Layout>
   )
 }
