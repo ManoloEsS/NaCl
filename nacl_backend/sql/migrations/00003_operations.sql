420f31bacbae32ba145f61210f51e56a163cd22d
ManoloEsS <manolo.estrada.s@gmail.com>
2026-06-21 17:39:11 -0600

    fix: modify operations schema and queries

--- nacl_backend/sql/migrations/00003_operations.sql
+++ nacl_backend/sql/migrations/00003_operations.sql
@@ -2,10 +2,9 @@
 CREATE TABLE operations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
     user_id UUID NOT NULL,
-    type TEXT NOT NULL,
+    op_type TEXT NOT NULL,
     service TEXT NOT NULL,
-    service_id UUID NOT NULL,
-    description TEXT,
+    service_id UUID,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT fk_service FOREIGN KEY (service_id)
     REFERENCES services(id),
