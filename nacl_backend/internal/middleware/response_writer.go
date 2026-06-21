f4494c9fe16897474b63cf31557a359a7da67d99
ManoloEsS <manolo.estrada.s@gmail.com>
2026-06-21 17:40:21 -0600

    refactor: extract response writer struct to own file

---
+++ nacl_backend/internal/middleware/response_writer.go
@@ -0,0 +1,30 @@
+package middleware
+
+import "net/http"
+
+type responseWriter struct {
+	http.ResponseWriter
+	status      int
+	wroteHeader bool
+}
+
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{ResponseWriter: w, status: 200}
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	if rw.wroteHeader {
+		return
+	}
+	rw.status = code
+	rw.wroteHeader = true
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	if !rw.wroteHeader {
+		rw.status = 200
+		rw.wroteHeader = true
+	}
+	return rw.ResponseWriter.Write(b)
+}
