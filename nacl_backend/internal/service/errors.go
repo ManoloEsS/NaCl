157017fe7b2c3cf674ac3495c74c30ca7216461b
ManoloEsS <manolo.estrada.s@gmail.com>
2026-06-26 15:23:09 -0600

    feat: add unauthorized error to service errors

--- nacl_backend/internal/service/errors.go
+++ nacl_backend/internal/service/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
 	ErrUserNotFound       = errors.New("user not found")
 	ErrCredentialNotFound = errors.New("credential not found")
+	ErrUnauthorized       = errors.New("credentials do not match")
 )
