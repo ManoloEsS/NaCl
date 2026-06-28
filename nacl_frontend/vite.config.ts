c50bd490f9730abc185cbdfc70cb1a92561c6662
ManoloEsS <manolo.estrada.s@gmail.com>
2026-06-27 20:16:42 -0600

    fix: change frontend build destination

--- nacl_frontend/vite.config.ts
+++ nacl_frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  build: { outDir: '../nacl_backend/dist' },
+  build: { outDir: '../nacl_backend/static' },
   server: {
     proxy: {
       '/api/': {
